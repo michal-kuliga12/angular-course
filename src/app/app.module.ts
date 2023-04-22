7355ac13516b5dd625c613fb70329a6606d025b9
Michał Kuliga <mkuliga72@gmail.com>
2023-04-22 19:32:50 +0200

    #10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
-  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent],
+  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent, NotificationComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
