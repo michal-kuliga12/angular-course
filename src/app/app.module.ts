00d29bde494b9f692cae101e16bb2f4e8216fd42
Michał Kuliga <mkuliga72@gmail.com>
2023-05-17 22:33:32 +0200

    view-encapsulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule
